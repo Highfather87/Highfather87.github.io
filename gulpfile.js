3222cc8df4761ee16467f9c680628728efc83d06
Mark Mushiva <mmushiva@gmail.com>
2019-10-31 08:50:17 +0200

    updated about description

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,68 @@
-//Working version Mark Mushiva 02/09/2017
+var browsersync = require('browser-sync');
+var cp = require('child_process');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
 
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
-var jade        = require('gulp-jade');
-
-
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
-var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+var paths = {
+html: {
+src: './app//*.html',
+},
+jekyll: {
+src: './app',
+dist: './_site',
+},
+markdown: {
+src: './app//.markdown',
+},
+styles: {
+src: './app/_sass/**/.scss',
+dist: './app/css',
+}
 };
 
-/**
- * Build the Jekyll Site
- */
-gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-        .on('close', done);
-});
+gulp.task(jekyllbuild);
+gulp.task('jekyllrebuild', gulp.series(jekyllbuild, function(cb) {
+browsersync.reload();
+cb();
+}));
+gulp.task(serve);
+gulp.task(styles);
+gulp.task(watch);
+gulp.task('default',
+gulp.series(
+styles,
+jekyllbuild,
+gulp.parallel(serve, watch))
+);
 
-/**
- * Rebuild Jekyll & do page reload
- */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
-});
+function jekyllbuild(done) {
+browsersync.notify('building jekyll');
+return cp.spawn('jekyll.bat', ['build'], {stdio: 'inherit'})
+.on('close', done);
+}
 
-/**
- * Wait for jekyll-build, then launch the Server
- */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-       , notify:false  //in order to remove notification
-    });
-});
+function jekyllrebuild() {
+browsersync.reload();
+};
 
-/**
- * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
- */
-gulp.task('sass', function () {
-    return gulp.src('assets/css/main.scss')
-        .pipe(sass({
-            includePaths: ['css'],
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/assets/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('assets/css'));
-});
+function serve() {
+browsersync.init({
+server: paths.jekyll.dist,
+notify: true
+})
+}
 
-/**
- * Gulp function to compile jade files into html files in the includes folder
- */
- gulp.task('jade', function () {
-    return gulp.src('_jadefiles/*.jade')
-    .pipe(jade())
-    .pipe(gulp.dest('_includes'));
- });
+function styles() {
+return gulp.src(paths.styles.src)
+.pipe(sass().on('error', sass.logError))
+.pipe(gulp.dest(paths.jekyll.dist + '/css'))
+.pipe(browsersync.stream())
+.pipe(gulp.dest(paths.styles.dist))
+}
 
-
-/**
- * Watch scss files for changes & recompile
- * Watch html/md files, run jekyll & reload BrowserSync
- */
-gulp.task('watch', function () {
-    gulp.watch(['assets/css/**'], ['sass']);
-    gulp.watch('assets/js/**', ['jekyll-rebuild']);
-    gulp.watch(['*.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
-    //gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
-    gulp.watch(['assets/js/**'], ['jekyll-rebuild']);
-    gulp.watch(['_jadefiles/*.jade'], ['jade']);
-});
-
-/**
- * Default task, running just `gulp` will compile the sass,
- * compile the jekyll site, launch BrowserSync & watch files.
- */
-gulp.task('default', ['browser-sync', 'watch']);
+function watch() {
+gulp.watch(paths.styles.src, styles);
+gulp.watch(paths.html.src, gulp.series('jekyllrebuild'));
+gulp.watch(paths.markdown.src, gulp.series('jekyllrebuild'));
+}
